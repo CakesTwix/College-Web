2d9462f35f5e0460f4f6d864b0590f8aef44fb60
CakesTwix <oleg.kiryazov@gmail.com>
2020-11-28 21:19:49 +0200

    Added DB

---
+++ College-Web/Models/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace College_Web.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) { }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ College-Web/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace College_Web.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        override public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
