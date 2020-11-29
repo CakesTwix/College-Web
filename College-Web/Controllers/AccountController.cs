07df7be56cdf44827580b00d3dcec0b56f298433
CakesTwix <oleg.kiryazov@gmail.com>
2020-11-29 10:26:18 +0200

    Account: Add Login

--- College-Web/Controllers/AccountController.cs
+++ College-Web/Controllers/AccountController.cs
@@ -2,16 +2,19 @@
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using College_Web.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Identity.Controllers
 {
     public class AccountController : Controller
     {
         private UserManager<UserApp> userManager;
+        private SignInManager<UserApp> signInManager;
 
-        public AccountController(UserManager<UserApp> usrMgr)
+        public AccountController(UserManager<UserApp> usrMgr, SignInManager<UserApp> signinMgr)
         {
             userManager = usrMgr;
+            signInManager = signinMgr;
         }
 
         public ActionResult Index()
@@ -19,6 +22,34 @@ namespace Identity.Controllers
             return View();
         }
         public ViewResult Create() => View();
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            Login login = new Login();
+            login.ReturnUrl = returnUrl;
+            return View(login);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                UserApp appUser = await userManager.FindByEmailAsync(login.Email);
+                if (appUser != null)
+                {
+                    await signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(appUser, login.Password, false, false);
+                    if (result.Succeeded)
+                        return Redirect(login.ReturnUrl ?? "/");
+                }
+                ModelState.AddModelError(nameof(login.Email), "Login Failed: Invalid Email or password");
+            }
+            return View(login);
+        }
 
         [HttpPost]
         public async Task<ActionResult> Create(UserApp user)
