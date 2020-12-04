Roker2/RockBotPanel

Role: Add capability to add and remove users from role

RockBotPanel/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using RockBotPanel.ViewModels.Role;
 using RockBotPanel.Models;
+using System.Collections.Generic;
 
 namespace RockBotPanel.Controllers
 {
@@ -153,6 +154,73 @@ namespace RockBotPanel.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            IdentityRole role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            List<UserRoleViewModel> model = new List<UserRoleViewModel>();
+
+            foreach (var user in userManager.Users)
+            {
+                model.Add(new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    IsInRole = await userManager.IsInRoleAsync(user, role.Name)
+                });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            foreach(UserRoleViewModel item in model)
+            {
+                TelegramUser user = await userManager.FindByIdAsync(item.UserId);
+
+                IdentityResult result;
+                if (item.IsInRole && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!item.IsInRole && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (!result.Succeeded)
+                {
+                    ViewBag.ErrorMessage = $"Can not save role for {item.UserName}";
+                    return View("NotFound");
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
         }
     }
 }

CakesTwix/College-Web

College-Web/Controllers/RoleController.cs
@@ -2,9 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using College_Web.Models;
-using RockBotPanel.ViewModels;
+using System.Collections.Generic;
 
-namespace RockBotPanel.Controllers
+namespace College_Web.Controllers
 {
     public class RoleController : Controller
     {
@@ -88,6 +88,73 @@ namespace RockBotPanel.Controllers
             }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            IdentityRole role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            List<UserRoleViewModel> model = new List<UserRoleViewModel>();
+
+            foreach (var user in userManager.Users)
+            {
+                model.Add(new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    IsInRole = await userManager.IsInRoleAsync(user, role.Name)
+                });
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            var role = await roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} cannot be found";
+                return View("NotFound");
+            }
+
+            foreach(UserRoleViewModel item in model)
+            {
+                UserApp user = await userManager.FindByIdAsync(item.UserId);
+
+                IdentityResult result;
+                if (item.IsInRole && !(await userManager.IsInRoleAsync(user, role.Name)))
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!item.IsInRole && await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (!result.Succeeded)
+                {
+                    ViewBag.ErrorMessage = $"Can not save role for {item.UserName}";
+                    return View("NotFound");
+                }
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId });
         }
     }
 }